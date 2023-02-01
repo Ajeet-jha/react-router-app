59a786a592a2d3dbdcb2cab851e7b3097332d8d0
Ajeet kumar jha <jhaajeetcs@gmail.com>
2023-02-01 04:48:56 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { routers } from "./routes";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>CodeSandbox react-router-dom: "6.8.0" </h1>
       <RouterProvider router={routers} />
     </div>
   );
